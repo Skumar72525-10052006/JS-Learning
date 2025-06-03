d49548b290dc77a9a5cf9b243caa2763a546a9b7
Sonu Kumar <sonukumarpaw@gmail.com>
2025-06-03 16:08:55 +0000

    Run A Program

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+// todo If U Want To Treat Your JS File As A Newer Version You Can Use The Command Given Below :- 
+// * "use strict";
+
+// ! alert(3 + 3) We Can't Use Alert Inside NodeJS Engine, It Can Be Used Inside Browser Engine.
+
+// ^ Make Sure To Increase The Code Readability.
+
+// ? What Is Data Types ????
+
+// !--> It Is A Classification That Tells The Computer How To Interpret And Process A Piece Of Data. It Defines The Type Of Value A Variable Can Hold.
+
+/*
+    & JS Supports 2 Types Of Data :-
+    1) Primitive
+    2) Non-Primitive
+*/
+
+/*
+
+*/
